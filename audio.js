fbe42feb9c78a62ba398e21734476624a7bc3bfd
thipokch <thipok.ch@gmail.com>
2017-05-20 22:10:37 -0700

    Add paintVisual

--- audio.js
+++ audio.js
@@ -77,7 +77,8 @@ function setupAnalyser () {
 
  canvasContext.clearRect(0, 0, canvas.width, canvas.height)
 
- drawStandardVisual()
+ paintVisual()
+//    drawStandardVisual()
 }
 
 function drawStandardVisual () {
@@ -92,13 +93,64 @@ function drawStandardVisual () {
   var x = 0
 
   for (var i = 0; i < bufferLength; i++) {
-    barHeight = dataArray[i] * canvas.height / 255
-    intensity = dataArray[i] / 255
+    barHeight = dataArray[i] * canvas.height / 100
+    intensity = dataArray[i] / 100
     canvasContext.fillStyle = 'rgb(' + 255*intensity + ',' + 50 + ',' + 255*(1-intensity)
     canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
 
     x += barWidth
   }
+}
+
+function paintVisual () {
+var drawVisual = requestAnimationFrame(paintVisual)
+analyser.getByteFrequencyData(dataArray)
+
+ canvasContext.fillStyle = 'rgb(0,0,0)'
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+  var barWidth = (canvas.width / bufferLength)
+  var barHeight
+
+  for (var i = 0; i < bufferLength; i+= 10) {
+      
+    third = dataArray[bufferLength *  6/ 8] / 255 
+      second = dataArray[bufferLength * 4 / 8] / 255
+      first = dataArray[bufferLength * 2 / 8] / 255
+      
+    
+      
+    canvasContext.fillStyle = 'rgb(' + 255* first + ',' + 255 * second + ',' + 255* third + ')'
+      canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+      
+      canvasContext.strokeStyle = 'rgb(255,255,255)';
+      
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'rgb(' + 255* second + ',' + 255 * first + ',' + 255* third + ')'
+      canvasContext.arc(canvas.width / 2,canvas.height / 2 ,second * canvas.height + 300,0,2*Math.PI);
+      canvasContext.fill();
+      canvasContext.stroke();
+      
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'rgb(' + 255* third + ',' + 255 * first + ',' + 255* second + ')'
+      canvasContext.arc(canvas.width / 2,canvas.height / 2 ,second * canvas.height + 200,0,2*Math.PI);
+      canvasContext.fill();
+      canvasContext.stroke();
+      
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'rgb(' + 255* second + ',' + 255 * third + ',' + 255* first + ')'
+      canvasContext.arc(canvas.width / 2,canvas.height / 2 ,second * canvas.height + 100,0,2*Math.PI);
+      canvasContext.fill();
+      canvasContext.stroke();
+      
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'rgb(' + 255* third + ',' + 255 * second + ',' + 255* first + ')'
+      canvasContext.arc(canvas.width / 2,canvas.height / 2,second * canvas.height,0,2*Math.PI);
+      canvasContext.fill();
+      canvasContext.stroke();
+      
+  }
+    
+    
 }
 
 function hide (elements) {
