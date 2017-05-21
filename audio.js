af103513a30f3b3de1d9bde909a0833986f5bbda
Tenma Rollins <tenmarollins@gmail.com>
2017-05-21 00:58:27 -0700

    reformatted

--- audio.js
+++ audio.js
@@ -1,106 +1,42 @@
+var source
+var audioContext = new (window.AudioContext || window.webkitAudioContext)()
+var analyser = audioContext.createAnalyser()
+analyser.fftSize = 32
+var dataArray = new Uint8Array(analyser.fftSize)
+var bufferLength = analyser.frequencyBinCount
+
 var canvas
 var canvasContext
 
-var audioContext
-var source
-var sourceStream
-var sourceBuffer
-var analyser
-
-var bufferLength
-var dataArray
-var useMic = true;
-
-// loading stuff in on page load
 window.onload = function () {
-  canvas = document.getElementById('AudioVisual')
-  canvas.focus()
-  canvasContext = canvas.getContext('2d')
-
-  canvasContext.fillStyle = 'black'
-  canvasContext.fillRect(0,0,canvas.width,canvas.height)
-
-  audioContext = new (window.AudioContext || window.webkitAudioContext)()
-  analyser = audioContext.createAnalyser()
-
-  if (useMic) {
-    hide(document.getElementById("audioControls"))
-    micLoad();
-  } else {
-    document.querySelector('input').addEventListener('change', fileLoad)
-  }
-}
-
-function micLoad () {
     navigator.getUserMedia(
-          {"audio": true},
-           gotStream, function(e) {
-              alert('Error getting audio');
-              console.log(e);
-          });
-
-  function gotStream(stream) {
-    var source = audioContext.createMediaStreamSource(stream);
-    source.connect(analyser)
-      //analyser.connect(audioContext.destination)
-      setupAnalyser()
-  }
+        {"audio": true},
+        function gotStream(stream) {
+            source = audioContext.createMediaStreamSource(stream);
+            source.connect(analyser);
+            setup();
+        }, 
+        function(e) { 
+            alert('Error getting audio'); 
+            console.log(e); 
+        }
+    );
 }
 
-// loads the sound into buffer when you upload
-function fileLoad () {
-  var fileReader = new FileReader()
-  fileReader.onload = function () {
-    sourceBuffer = this.result
-  }
-
-  fileReader.readAsArrayBuffer(this.files[0])
-
-  var url = URL.createObjectURL(this.files[0])
-  audio_player.src = url
-
-  audio_player.play()
-  var source = audioContext.createMediaElementSource(document.querySelector('audio'))
-  
-  source.connect(analyser)
-  analyser.connect(audioContext.destination)
-
-  setupAnalyser()
+function setup() {
+    console.log("hi")
+    canvas = document.getElementById('canvas0')
+    canvas.focus()
+    canvasContext = canvas.getContext('2d')
+    
+    setInterval(update, 10)
 }
-
-function setupAnalyser () {
- analyser.fftSize = Math.pow(2, Math.floor(Math.log2(canvas.width)))
- bufferLength = analyser.frequencyBinCount
- dataArray = new Uint8Array(bufferLength)
-
- analyser.getByteTimeDomainData(dataArray)
-
- canvasContext.clearRect(0, 0, canvas.width, canvas.height)
-
-    // drawColorCircles()
+var max = 0
+function update() {
+    analyser.getByteFrequencyData(dataArray)
+    //drawColorCircles()
+    //drawStandardVisual()
     drawBubbleVisual()
-//    drawStandardVisual()
-}
-
-function drawStandardVisual () {
-  var drawVisual = requestAnimationFrame(drawStandardVisual)
-
-  analyser.getByteFrequencyData(dataArray)
-
-  canvasContext.fillStyle = 'rgb(0,0,0)'
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height)
-  var barWidth = (canvas.width / bufferLength)
-  var barHeight
-  var x = 0
-
-  for (var i = 0; i < bufferLength; i++) {
-    barHeight = dataArray[i] * canvas.height / 100
-    intensity = dataArray[i] / 100
-    canvasContext.fillStyle = 'rgb(' + 255*intensity + ',' + 50 + ',' + 255*(1-intensity)
-    canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
-
-    x += barWidth
-  }
 }
 
 function drawColorCircles () {
@@ -147,81 +83,71 @@ function drawColorCircles () {
     }
 }
 
-  function drawStandardVisual () {
-      var drawVisual = requestAnimationFrame(drawStandardVisual)
+function drawStandardVisual () {
+    var drawVisual = requestAnimationFrame(drawStandardVisual)
 
-      analyser.getByteFrequencyData(dataArray)
+    analyser.getByteFrequencyData(dataArray)
 
-      canvasContext.fillStyle = 'rgb(0,0,0)'
-      canvasContext.fillRect(0, 0, canvas.width, canvas.height)
-      var barWidth = (canvas.width / bufferLength)
-      var barHeight
-      var x = 0
+    canvasContext.fillStyle = 'rgb(0,0,0)'
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    var barWidth = (canvas.width / bufferLength)
+    var barHeight
+    var x = 0
 
-      for (var i = 0; i < bufferLength; i++) {
-          barHeight = dataArray[i] * canvas.height / 100
-          intensity = dataArray[i] / 100
-          canvasContext.fillStyle = 'rgb(' + 255*intensity + ',' + 50 + ',' + 255*(1-intensity)
-          canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
+    for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i] * canvas.height / 100
+        intensity = dataArray[i] / 100
+        canvasContext.fillStyle = 'rgb(' + 255*intensity + ',' + 50 + ',' + 255*(1-intensity)
+        canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
 
-          x += barWidth
-      }
-  }
+        x += barWidth
+    }
+}
 
-  function drawBubbleVisual () {
-      var drawVisual = requestAnimationFrame(drawBubbleVisual)
+function drawBubbleVisual () {
+    var drawVisual = requestAnimationFrame(drawBubbleVisual)
 
-      analyser.getByteFrequencyData(dataArray)
+    analyser.getByteFrequencyData(dataArray)
 
-      canvasContext.fillStyle = 'rgb(0,0,0)'
-      canvasContext.fillRect(0, 0, canvas.width, canvas.height)
-      var barWidth = (canvas.width / bufferLength)
-      var barHeight
-      var x = 0
+    canvasContext.fillStyle = 'rgb(0,0,0)'
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    var barWidth = (canvas.width / bufferLength)
+    var barHeight
+    var x = 0
 
-      for (var i = 0; i < bufferLength; i+= 100) {
+    for (var i = 0; i < bufferLength; i+= 100) {
 
-          third = dataArray[bufferLength *  7/ 8] / 255
-          second = dataArray[bufferLength * 4 / 8] / 255
-          first = dataArray[bufferLength * 1 / 8] / 255
+        third = dataArray[bufferLength *  7/ 8] / 255
+        second = dataArray[bufferLength * 4 / 8] / 255
+        first = dataArray[bufferLength * 1 / 8] / 255
 
-          canvasContext.fillStyle = 'rgb(' + 255* first + ',' + 255 * second + ',' + 255* third + ')'
-          canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+        canvasContext.fillStyle = 'rgb(' + 255* first + ',' + 255 * second + ',' + 255* third + ')'
+        canvasContext.fillRect(0, 0, canvas.width, canvas.height)
 
 //      canvasContext.strokeStyle = 'rgb(255,255,255)';
 
-          canvasContext.beginPath();
-          canvasContext.fillStyle = 'rgb(' + 255* second + ',' + 255 * first + ',' + 255* third + ')'
-          canvasContext.arc(canvas.width * Math.random(),canvas.height * Math.random() ,first * canvas.height,0,2*Math.PI);
-          canvasContext.fill();
+        canvasContext.beginPath();
+        canvasContext.fillStyle = 'rgb(' + 255* second + ',' + 255 * first + ',' + 255* third + ')'
+        canvasContext.arc(canvas.width * Math.random(),canvas.height * Math.random() ,first * canvas.height,0,2*Math.PI);
+        canvasContext.fill();
 //      canvasContext.stroke();
 
-          canvasContext.beginPath();
-          canvasContext.fillStyle = 'rgb(' + 255* third + ',' + 255 * first + ',' + 255* second + ')'
-          canvasContext.arc(canvas.width * Math.random(),canvas.height * Math.random() ,third * canvas.height,0,2*Math.PI);
-          canvasContext.fill();
+        canvasContext.beginPath();
+        canvasContext.fillStyle = 'rgb(' + 255* third + ',' + 255 * first + ',' + 255* second + ')'
+        canvasContext.arc(canvas.width * Math.random(),canvas.height * Math.random() ,third * canvas.height,0,2*Math.PI);
+        canvasContext.fill();
+//      canvasContext.stroke();
+        canvasContext.beginPath();
+        canvasContext.fillStyle = 'rgb(' + 255* second + ',' + 255 * third + ',' + 255* first + ')'
+        canvasContext.arc(canvas.width * Math.random(),canvas.height * Math.random() ,second * canvas.height,0,2*Math.PI);
+        canvasContext.fill();
 //      canvasContext.stroke();
 
-          canvasContext.beginPath();
-          canvasContext.fillStyle = 'rgb(' + 255* second + ',' + 255 * third + ',' + 255* first + ')'
-          canvasContext.arc(canvas.width * Math.random(),canvas.height * Math.random() ,second * canvas.height,0,2*Math.PI);
-          canvasContext.fill();
-//      canvasContext.stroke();
-
-          canvasContext.beginPath();
-          canvasContext.fillStyle = 'rgb(' + 255* third + ',' + 255 * second + ',' + 255* first + ')'
-          canvasContext.arc(canvas.width * Math.random(),canvas.height * Math.random(),second * canvas.height,0,2*Math.PI);
-          canvasContext.fill();
+        canvasContext.beginPath();
+        canvasContext.fillStyle = 'rgb(' + 255* third + ',' + 255 * second + ',' + 255* first + ')'
+        canvasContext.arc(canvas.width * Math.random(),canvas.height * Math.random(),second * canvas.height,0,2*Math.PI);
+        canvasContext.fill();
 //      canvasContext.stroke()
-      }
+    }
 
-  }
-
-
-function hide (elements) {
-  elements = elements.length ? elements : [elements];
-  for (var index = 0; index < elements.length; index++) {
-    elements[index].style.display = 'none';
-  }
 }
-
